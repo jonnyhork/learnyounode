eab5e9bf11908dbaef22f7f13e5d6694bef45881
Jonny Hork <JonnyHork@gmail.com>
2017-09-11 20:56:29 -0600

    HTTP client done

---
+++ httpGet.js
@@ -0,0 +1,34 @@
+/*
+Write the String contents of each
+"data" event from the response to a new line on the console (stdout).
+*/
+
+const http = require('http')
+const url = process.argv[2] // first cli arg, url
+
+http.get(url, (response) => {
+  // console.log('RESPONSE = ', response);
+  response.setEncoding('utf8')
+  response.on("data", (data) => {
+    console.log(data);
+  })
+})
+
+
+
+
+
+
+
+// POSSIBLE SOLUTION
+
+/*
+const http = require('http')
+
+    http.get(process.argv[2], function (response) {
+      response.setEncoding('utf8')
+      response.on('data', console.log)
+      response.on('error', console.error)
+    }).on('error', console.error)
+
+*/
